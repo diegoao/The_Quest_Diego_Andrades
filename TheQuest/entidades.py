e42cc788b6d065fed848c9788a8251121bf56f7b
diegoao <die_94_go@hotmail.com>
2023-10-15 21:16:01 +0200

    feat: Aumento la velocidada de forma progresiva si dejamos la tecla pulsada

--- TheQuest/entidades.py
+++ TheQuest/entidades.py
@@ -10,17 +10,11 @@ from . import ALTO,  ANCHO
 
 
 class NaveEspacial(pg.sprite.Sprite):
-    """
-    1. Debe ser de tipo Sprite (herencia) -- DONE
-    2. Se puede mover (método)
-        2.1 Leer el teclado
-        2.2 Límites de movimiento (no debe salir de la pantalla)
-    3. Pintarse (método) -- DONE
-    4. Volver a la posición inicial (método)
-    5. Velocidad  --- DONE
-    """
+
     margen = 100
-    velocidad = 20
+    velocidadMin = 20
+    velocidadMax = 60
+    aumentoVelo = 2
 
     def __init__(self):
         super().__init__()
@@ -36,21 +30,27 @@ class NaveEspacial(pg.sprite.Sprite):
         self.image = self.imagenes[self.contador]
         anchuraNave = self.image.get_width()
         self.rect = self.image.get_rect(midbottom=(anchuraNave/2, ALTO/2))
+        self.velomovimiento = self.velocidad_min
 
     def update(self):
         # 00 -> 01 -> 00 -> 01
-        self.contador += 1
         alturaNave = self.image.get_height()
         if self.contador > 1:
             self.contador = 0
         self.image = self.imagenes[self.contador]
-
         pulsadas = pg.key.get_pressed()
+
+        if ((pulsadas[pg.K_UP] and not pulsadas[pg.K_DOWN]) or (pulsadas[pg.K_DOWN] and not pulsadas[pg.K_UP])) and self.velomovimiento < self.velocidadMax:
+            self.velomovimiento += self.aumentoVelo
+        elif (not pulsadas[pg.K_UP] and not pulsadas[pg.K_DOWN]) or (pulsadas[pg.K_UP] and pulsadas[pg.K_DOWN]):
+            self.velomovimiento = self.velocidadMin
+        print(self.velomovimiento)
+
         if pulsadas[pg.K_UP]:
-            self.rect.y -= self.velocidad
+            self.rect.y -= self.velomovimiento
             if self.rect.bottom < alturaNave:
                 self.rect.bottom = alturaNave
         if pulsadas[pg.K_DOWN]:
-            self.rect.y += self.velocidad
+            self.rect.y += self.velomovimiento
             if self.rect.top > ALTO-alturaNave:
                 self.rect.top = ALTO-alturaNave
